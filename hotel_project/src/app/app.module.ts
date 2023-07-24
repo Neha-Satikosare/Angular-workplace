95fd42190c01e1cac6d3f6dab272f767c6c553ba
Your Name <you@example.com>
2023-07-24 19:40:08 +0530

    admin user

--- hotel_project/src/app/app.module.ts
+++ hotel_project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { OwnerComponent } from './owner/owner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    OwnerComponent,
   ],
   imports: [
     BrowserModule,
